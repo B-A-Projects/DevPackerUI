92216bcd06f111cdcdbd0219e62a1db4b867de17
Bluerrito <yannick.d.b114@gmail.com>
2024-07-10 14:55:58 +0200

    Initial Commit

---
+++ DevPackerUI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DevPackerUI
+//
+//  Created by Yannick de Boer on 10/07/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
